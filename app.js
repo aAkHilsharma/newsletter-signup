daea2880f46d960bd0fd9ccf3428e652ba757e69
Akhil Sharma <akhilsharma6721@gmail.com>
2023-05-09 18:18:11 +0530

    updated port

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/', function (req, res) {
 app.post('/failure', function (req, res) {
   res.redirect('/');
 });
-
-app.listen(3000, function () {
-  console.log('Server running on port 3000...');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server running on port ${port}...`);
 });
