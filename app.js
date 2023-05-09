c3762e520ad84d189fdb38c4f2ecc48b30f2c0fc
Akhil Sharma <akhilsharma6721@gmail.com>
2023-05-09 17:57:45 +0530

    file modified

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/signup.html');
 });
 app.post('/', function (req, res) {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
+  const { firstName, lastName, email } = req.body;
 
   const data = {
     members: [
@@ -30,20 +28,28 @@ app.post('/', function (req, res) {
   };
 
   const jsonData = JSON.stringify(data);
-  console.log(jsonData);
   const url = 'https://us10.api.mailchimp.com/3.0/lists/7755ae94f1';
   const options = {
     method: 'POST',
-    auth: 'auth 3eb16b62a95975e4dbc8d06a5f93d099-us10',
+    auth: 'akhil1:eafafd873edd8df02ca5e6ff6e9ef72c-us10',
   };
 
   const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + '/success.html');
+    } else {
+      res.sendFile(__dirname + '/failure.html');
+    }
     response.on('data', function (data) {
       console.log(JSON.parse(data));
     });
   });
   request.write(jsonData);
   request.end();
+});
+
+app.post('/failure', function (req, res) {
+  res.redirect('/');
 });
 
 app.listen(3000, function () {
